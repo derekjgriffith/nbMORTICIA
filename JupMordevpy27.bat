685ff5eb0da939e8ce26a2e68e6a4f15cd39ca35
Derek Griffith <dgriffith@csir.co.za>
2015-12-23 13:25:18 +0200

    Updated virtualenv Batch Scripts

--- JupMordevpy27.bat
+++ JupMordevpy27.bat
@@ -1,10 +1,16 @@
-REM Activate the Python 2 Development environment for MORTICIA in Windows
+REM Activate the Python 2.7 Development environment for MORTICIA in Windows
 REM There must be a morticia.pth file in \Anaconda\envs\mordevpy2\Lib\site-packages\ that points
 REM to the root directory of your clone of the the MORTICIA GitHub repository
 REM for example, morticia.pth should have the single line D:\\Projects\\GitHub\\MORTICIA
-REM The activate command is a batch file and must therefore be wrapped in a call command
-call activate mordevpy27
 
+REM NOTE : Switching environments does not work for notebooks unless Jupyter is installed
+REM        in the environment which you switch to. In this case, Jupyter must be installed
+REM        in the mordevpy27 environment.
+
+REM The activate command is a batch file and must therefore be wrapped in a call command 
+call activate mordevpy27
+REM Get Jupyter pointing in the right direction
+jupyter kernelspec install-self --user
 REM Note that the --script option is now deprecated and this should be done in some other way
 jupyter notebook --script
 
--- JupMordevpy33.bat
+++ JupMordevpy33.bat
@@ -2,6 +2,11 @@ REM Activate the Python 3.3 Development environment for MORTICIA in Windows
 REM There must be a morticia.pth file in \Anaconda\envs\mordevpy2\Lib\site-packages\ that points
 REM to the root directory of your clone of the the MORTICIA GitHub repository
 REM for example, morticia.pth should have the single line D:\\Projects\\GitHub\\MORTICIA
+
+REM NOTE : Switching environments does not work for notebooks unless Jupyter is installed
+REM        in the environment which you switch to. In this case, Jupyter must be installed
+REM        in the mordevpy33 environment.
+
 REM The activate command is a batch file and must therefore be wrapped in a call command
 call activate mordevpy33
 REM Get Jupyter pointing in the right direction
