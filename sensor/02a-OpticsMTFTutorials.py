96d88f57aeac61c1d97abb7481925e6ce9f32bd4
Derek Griffith <dgriffith@csir.co.za>
2015-12-24 10:44:07 +0200

    Fixed OpticsMTFTut

--- sensor/02a-OpticsMTFTutorials.py
+++ sensor/02a-OpticsMTFTutorials.py
@@ -5,29 +5,25 @@
 
 # The following tutorials show the usage of various lens-related functions in the optics.py module, which implements a range of functions related to optical systems and the human eye. Many of these functions are used in MORTICIA.
 
-# In[1]:
+# In[6]:
 
 # Perform the standard numpy and units imports
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+get_ipython().magic(u'matplotlib inline')
 from pint import UnitRegistry
 ureg = UnitRegistry()
 Q_ = ureg.Quantity
-import matplotlib.pyplot as plt
-get_ipython().magic(u'matplotlib inline')
-
 import sys
-#sys.path.append('../sensor')  # Remove this call if the optics module is already installed elsewhere 
-import optics
-# This notebook is used for development/testing of the Optics module, so auto reload the Optics module if it changes
+import morticia.sensor.optics as optics
+# This notebook is also used for development/testing of the Optics module, so auto reload the Optics module if it changes
 get_ipython().magic(u'load_ext autoreload')
-get_ipython().magic(u'aimport optics')
+get_ipython().magic(u'aimport morticia.sensor.optics')
 get_ipython().magic(u'autoreload 1')
 
-# If you are pacticipating in development of this notebook, please start the notebook with the --script option
 
-
-# In[ ]:
+# In[7]:
 
 SpatialFrequencies = Q_(np.arange(0.0, 2200.0, 50.0), '1/millimeter') # cycles per millimeter
 Wavelengths = Q_([0.001, 0.0005], 'millimeter') # Wavelengths expressed in mm as well to get valid MTF calculation
@@ -35,7 +31,7 @@ FocalRatios = Q_(1.0, '')
 MTF = optics.mtf(spf=SpatialFrequencies, wvl=Wavelengths, fno=FocalRatios)
 
 
-# In[ ]:
+# In[8]:
 
 plt.figure(figsize=(10,8))
 plt.rc('text', usetex=True)  # Use TeX to render the labels in the plot
@@ -48,8 +44,9 @@ plt.legend(['$\lambda$=' + str(wvl) for wvl in Wavelengths])
 plt.grid()
 
 
-# In[ ]:
+# In[11]:
 
+# Calculate MTF of system with central obscuration
 SpatialFrequencies = Q_(np.arange(0.0, 1010.0, 10.0), '1/millimeter') # cycles per millimeter
 Wavelengths = Q_(0.001, 'millimeter') # Wavelengths expressed in mm as well to get valid MTF calculation
 FocalRatios = [1.0, 2.0]
@@ -58,8 +55,9 @@ MTF = optics.mtf(spf=SpatialFrequencies, wvl=Wavelengths, fno=FocalRatios)
 MTF = optics.mtf_obs(spf=SpatialFrequencies, wvl=Wavelengths, fno=FocalRatios, obs=Obscuration)
 
 
-# In[ ]:
+# In[13]:
 
+# Plot MTF of obscured system
 plt.figure(figsize=(10,8))
 plt.rc('text', usetex=True)  # Use TeX to render the labels in the plot
 plt.rc('font', family='serif', size=15)  # Default to a serif font
@@ -71,7 +69,7 @@ plt.legend(['F/' + str(fno) for fno in FocalRatios])
 plt.grid()
 
 
-# In[ ]:
+# In[14]:
 
 # Illustrate the effect of wavefront distortion on the MTF
 SpatialFrequencies = Q_(np.arange(0.0, 1010.0, 10.0), '1/millimeter') # cycles per millimeter
@@ -82,7 +80,7 @@ RMSWavefrontError = 0.18
 theATF = optics.atf(SpatialFrequencies, Wavelengths, FocalRatios, RMSWavefrontError)
 
 
-# In[ ]:
+# In[15]:
 
 plt.figure(figsize=(10,8))
 plt.rc('text', usetex=True)  # Use TeX to render the labels in the plot
@@ -95,7 +93,7 @@ plt.legend(['F/' + str(fno) for fno in FocalRatios])
 plt.grid()
 
 
-# In[ ]:
+# In[16]:
 
 # Now include aberration transfer function in an MTF calculation
 SpatialFrequencies = Q_(np.arange(0.0, 400.0, 1.0), '1/millimeter') # cycles per millimeter
@@ -109,9 +107,9 @@ PolyMTFwithObscurationAndWFE = optics.pmtf_obs_wfe(SpatialFrequencies, Wavelengt
                                                    WavelengthWeights, ObscurationRatio)
 
 
+# In[17]:
 
-# In[ ]:
-
+# Plot of MTF including aberrations
 plt.figure(figsize=(10,8))
 plt.rc('text', usetex=True)  # Use TeX to render the labels in the plot
 plt.rc('font', family='serif', size=15)  # Default to a serif font
@@ -123,7 +121,7 @@ plt.legend(['F/' + str(fno) for fno in FocalRatios])
 plt.grid()
 
 
-# In[ ]:
+# In[18]:
 
 # Illustrate calculation of polychromatic MTF
 Wavelengths = Q_(np.array([350.0, 450.0, 550.0, 650.0])/1e6, 'millimeter')  # mm
@@ -133,8 +131,9 @@ FocalRatios = np.array([1.4,2.0,2.8])
 PolyMTF = optics.pmtf(SpatialFrequencies, Wavelengths, FocalRatios, WavelengthWeights)
 
 
-# In[ ]:
+# In[20]:
 
+# Plot polychromatic MTF
 plt.figure(figsize=(10,8))
 plt.rc('text', usetex=True)  # Use TeX to render the labels in the plot
 plt.rc('font', family='serif', size=15)  # Default to a serif font
@@ -144,21 +143,6 @@ plt.ylabel('Polychromatic MTF')
 plt.title('Polychromatic Modulation Transfer Function')
 plt.legend(['F/' + str(fno) for fno in FocalRatios])
 plt.grid()
-
-
-# In[12]:
-
-from ..morticia.moglo import *
-
-
-# In[10]:
-
-import os
-
-
-# In[11]:
-
-os.path.abspath(os.curdir)
 
 
 # In[ ]:
