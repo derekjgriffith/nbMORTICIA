3a4c36afbc6e851ef25d8f039399acad23a2d967
Derek Griffith <dgriffith@csir.co.za>
2016-07-08 12:02:26 +0200

    Added shell scripts for starting Jupyter on Linux.

---
+++ Jup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This is a convenience batch file to run Jupyter notebooks in the root Anaconda environment.
+# Run this batch file in order to use the notebooks in the root Anaconda environment 
+# (not in a conda virtual environment) 
+# Deactivate any applicable virtual environment
+deactivate
+jupyter kernelspec install-self --user
+jupyter notebook
+
+
+
