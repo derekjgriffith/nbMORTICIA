726cb90b409e34c0c5ef1bb5b64bb42a27b31444
Derek Griffith <dgriffith@csir.co.za>
2016-07-08 15:47:15 +0200

    Added linux version of script to start Jupyter notebook in mordevpy27 environement.

---
+++ JupMordevpy27.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Activate the Python 2.7 Development environment for MORTICIA in Windows
+# There must be a morticia.pth file in /Anaconda/envs/mordevpy2/Lib/site-packages/ that points
+# to the root directory of your clone of the the MORTICIA GitHub repository
+# for example, morticia.pth might have the single line /home/jsmith/GitHub/MORTICIA
+
+# NOTE : Switching environments does not work for notebooks unless Jupyter is installed
+#        in the environment which you switch to. In this case, Jupyter must be installed
+#        in the mordevpy27 environment.
+
+activate mordevpy27
+# Get Jupyter pointing in the right direction
+jupyter kernelspec install-self --user
+jupyter notebook
+
+
+
